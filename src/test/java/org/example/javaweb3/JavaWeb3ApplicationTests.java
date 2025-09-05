d837e1d0f955b05f9ef9a9d62d1e1cd57dc4f995
yue2.feng <yue2.feng@ly.com>
2025-09-05 10:10:24 +0800

    1

---
+++ src/test/java/org/example/javaweb3/JavaWeb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaweb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeb3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
